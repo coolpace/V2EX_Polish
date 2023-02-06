e57573c2f841ae4ebfe406df27c615047a9497e8
陈梓聪 <20804846@dw.gdbbk.com>
2023-02-06 18:09:37 +0800

    Init

---
+++ .prettierrc.js
@@ -0,0 +1,1 @@
+module.exports = require('prefer-code-style/prettier')
---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [require.resolve('prefer-code-style/stylelint')],
+  rules: {
+    'selector-id-pattern': null,
+    'selector-class-pattern': null,
+    'no-descending-specificity': null,
+  },
+  ignoreFiles: ['node_modules/**/*.css'],
+}
