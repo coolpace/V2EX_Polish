e0411f54aa06c2d88a53152f28dae4ef9a44a0c8
LeoKu <czc12580520@gmail.com>
2023-02-13 21:24:04 +0800

    WIP

--- src/web-scripts/index.ts
+++ src/web-scripts/index.ts
@@ -7,7 +7,7 @@ import { addStyle } from './style'
   $('#Top .site-nav .tools > .top').addClass('effect-btn')
   $('#Main .tab').addClass('effect-btn')
   $('#Main .topic_buttons a.tb').addClass('effect-btn')
-  $('#Main .topic-link').attr('target', '_blank')
+  $('#Main .topic-link, .item_hot_topic_title > a, .item_node').attr('target', '_blank')
 }
 
 addStyle()
