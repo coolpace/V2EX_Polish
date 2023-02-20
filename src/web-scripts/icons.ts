1d758308df05e6c0c2f1b8163b8ba08d930c2ba7
LeoKu <czc12580520@gmail.com>
2023-02-21 00:06:45 +0800

    WIP

---
+++ src/web-scripts/icons.ts
@@ -0,0 +1,129 @@
+export const iconEmoji = `
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke-width="1.5"
+  stroke="currentColor"
+  style="width:18px; height:18px;"
+>
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
+  />
+</svg>
+`
+
+export const iconHeart = `
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke-width="1.5"
+  stroke="currentColor"
+>
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+  />
+</svg>
+`
+
+export const iconHide = `
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke-width="1.5"
+  stroke="currentColor"
+>
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+  />
+</svg>
+`
+
+export const iconReply = `
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke-width="1.5"
+  stroke="currentColor"
+>
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+  />
+</svg>
+`
+
+export const iconStar = `
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <polygon
+    points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
+  ></polygon>
+</svg>
+`
+
+export const iconTwitter = `
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <path
+    d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"
+  ></path>
+</svg>
+`
+
+export const iconIgnore = `
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <circle cx="12" cy="12" r="10"></circle>
+  <path d="M8 15h8"></path>
+  <path d="M8 9h2"></path>
+  <path d="M14 9h2"></path>
+</svg>
+`
+
+export const iconLove = `
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <path
+    d="M20.42 4.58a5.4 5.4 0 0 0-7.65 0l-.77.78-.77-.78a5.4 5.4 0 0 0-7.65 0C1.46 6.7 1.33 10.28 4 13l8 8 8-8c2.67-2.72 2.54-6.3.42-8.42z"
+  ></path>
+</svg>
+`
--- src/web-scripts/topic.ts
+++ src/web-scripts/topic.ts
@@ -9,6 +9,16 @@ import {
   topicContentBox,
   topicOwnerName,
 } from './globals'
+import {
+  iconEmoji,
+  iconHeart,
+  iconHide,
+  iconIgnore,
+  iconLove,
+  iconReply,
+  iconStar,
+  iconTwitter,
+} from './icons'
 
 export function popular() {
   topicContentBox.find('.topic_content a[href]').prop('target', '_blank')
@@ -95,55 +105,28 @@ export function popular() {
   }
 }
 
-const iconHeart = `
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  fill="none"
-  viewBox="0 0 24 24"
-  stroke-width="1.5"
-  stroke="currentColor"
->
-  <path
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-  />
-</svg>
-`
-
 export function replaceHeart() {
-  commentCells.find('.small.fade').addClass('heart-box').find('img[alt="❤️"]').replaceWith(`
-      <span class="icon-heart">
-        ${iconHeart}
-      </span>
-    `)
+  commentCells
+    .find('.small.fade')
+    .addClass('heart-box')
+    .find('img[alt="❤️"]')
+    .replaceWith(`<span class="icon-heart">${iconHeart}</span>`)
 }
 
 export function setControls() {
+  const replyBtn = $('<button class="normal button">回复<kbd>Cmd+Enter</kbd></button>').replaceAll(
+    $('#reply-box input[type="submit"]')
+  )
+
   const emoticons = ['😀', '🤡', '💩', '🤩', '😂', '😅', '🥳']
   const emoticonsContent = $(`
     <div class="v2p-emoticons">
       ${emoticons.map((emoji) => `<span>${emoji}</span>`).join('')}
     </div>
   `)
-  const emojiBtn = $(`
-    <button type="button" class="normal button">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        style="width:18px; height:18px;"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
-        />
-      </svg>
-    </button>
-  `).insertAfter('#reply_content + .flex-one-row > input[type="submit"]')
+  const emojiBtn = $(
+    `<button type="button" class="normal button">${iconEmoji}</button>`
+  ).insertAfter(replyBtn)
   const closeBtn = $('<button type="button" class="normal button">隐藏</button>')
   const tooltip = $('<div id="v2p-tooltip" role="tooltip"></div>')
     .append(emoticonsContent, closeBtn)
@@ -175,11 +158,7 @@ export function setControls() {
 
     const crtlContainer = $('<span class="v2p-controls">')
 
-    const thankIcon = $(`
-      <span class="v2p-control">
-        ${iconHeart}
-      </span>
-    `)
+    const thankIcon = $(`<span class="v2p-control">${iconHeart}</span>`)
 
     const thankArea = ctrlArea.find('> .thank_area')
     const thanked = thankArea.hasClass('thanked')
@@ -192,23 +171,7 @@ export function setControls() {
       const hide = thankEle.eq(0).removeClass('thank')
       const thank = thankEle.eq(1).removeClass('thank')
 
-      hide.html(`
-        <span class="v2p-control effect-btn" title="隐藏">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-            />
-          </svg>
-        </span>
-      `)
+      hide.html(`<span class="v2p-control effect-btn" title="隐藏">${iconHide}</span>`)
       // const [, param1, param2] = Array.from(onclickStr!.match(/ignoreReply\((.*?),(.*?)\)/)!)
 
       thankIcon.attr('title', '感谢').addClass('effect-btn')
@@ -219,23 +182,11 @@ export function setControls() {
 
     const reply = ctrlArea.find('a:last-of-type')
 
-    reply.find('> img[alt="Reply"]').replaceWith(`
-      <span class="v2p-control v2p-ac-reply effect-btn" title="回复">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
-          />
-        </svg>
-      </span>
-    `)
+    reply
+      .find('> img[alt="Reply"]')
+      .replaceWith(
+        `<span class="v2p-control v2p-ac-reply effect-btn" title="回复">${iconReply}</span>`
+      )
 
     crtlContainer.append(reply)
 
@@ -244,78 +195,11 @@ export function setControls() {
     ctrlArea.empty().append(crtlContainer, floorNum)
   })
 
-  $('#reply-box input[type="submit"]').attr('value', '发表回复')
-
   const topicBtn = $('.topic_buttons .tb').addClass('v2p-tb')
-  topicBtn.eq(0).append(`
-    <span class="v2p-tb-icon">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      >
-        <polygon
-          points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-        ></polygon>
-      </svg>
-    </span>
-  `)
-  topicBtn.eq(1).append(`
-    <span class="v2p-tb-icon">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      >
-        <path
-          d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"
-        ></path>
-      </svg>
-    </span>
-  `)
-  topicBtn.eq(2).append(`
-    <span class="v2p-tb-icon">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      >
-        <circle cx="12" cy="12" r="10"></circle>
-        <path d="M8 15h8"></path>
-        <path d="M8 9h2"></path>
-        <path d="M14 9h2"></path>
-      </svg>
-    </span>
-  `)
-  topicBtn.eq(3).append(`
-    <span class="v2p-tb-icon">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      >
-        <path
-          d="M20.42 4.58a5.4 5.4 0 0 0-7.65 0l-.77.78-.77-.78a5.4 5.4 0 0 0-7.65 0C1.46 6.7 1.33 10.28 4 13l8 8 8-8c2.67-2.72 2.54-6.3.42-8.42z"
-        ></path>
-      </svg>
-    </span>
-  `)
+  topicBtn.eq(0).append(`<span class="v2p-tb-icon">${iconStar}</span>`)
+  topicBtn.eq(1).append(`<span class="v2p-tb-icon">${iconTwitter}</span>`)
+  topicBtn.eq(2).append(`<span class="v2p-tb-icon">${iconIgnore}</span>`)
+  topicBtn.eq(3).append(`<span class="v2p-tb-icon">${iconLove}</span>`)
 }
 
 export function nestedComments() {
