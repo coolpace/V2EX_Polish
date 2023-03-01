791c22aef413f442d80ab4576c47743ad6ea1687
LeoKu <czc12580520@gmail.com>
2023-03-01 20:33:54 +0800

    WIP

--- src/userscripts/topic/comment.ts
+++ src/userscripts/topic/comment.ts
@@ -269,6 +269,48 @@ function insertEmojiBox() {
   })
 }
 
+/**
+ * 代码参考自：https://github.com/bjzhou/v2ex-base64-decoder/blob/master/index.js
+ */
+function parseBase64() {
+  const base64regex = /[A-z0-9+/=]+/g
+
+  // 已知以下字符串不能作为 base64 字符串，排除掉。
+  const excludeList = [
+    'bilibili',
+    'Bilibili',
+    'MyTomato',
+    'InDesign',
+    'Encrypto',
+    'encrypto',
+    'Window10',
+    'USERNAME',
+    'airpords',
+    'Windows7',
+  ]
+
+  $commentCells.find('.reply_content').each((_, cellDom) => {
+    cellDom.innerHTML = cellDom.innerHTML.replace(base64regex, (str) => {
+      // 先从格式规则上简单排除掉非 base64 字符串。
+      if (str.length < 8 || str.length % 4 !== 0) {
+        return str
+      }
+
+      // 再从排除列表中排除掉非 base64 字符串。
+      if (excludeList.includes(str)) {
+        return str
+      }
+
+      try {
+        const decodedStr = window.atob(str)
+        return `${str}(${decodedStr})`
+      } catch {
+        return str
+      }
+    })
+  })
+}
+
 export function handlingComments() {
   {
     /**
@@ -286,7 +328,13 @@ export function handlingComments() {
 
   {
     $commentCells.each((i, cellDom) => {
-      const currentComment = commentDataList.find((data) => data.id === cellDom.id)
+      const commentFromIndex = commentDataList.at(i)
+
+      // 先根据索引去找，如果能对应上就不需要再去 find 了，这样能加快处理速度。
+      const currentComment =
+        commentFromIndex?.id === cellDom.id
+          ? commentFromIndex
+          : commentDataList.find((data) => data.id === cellDom.id)
 
       if (currentComment) {
         const { refMemberNames, refFloors } = currentComment
