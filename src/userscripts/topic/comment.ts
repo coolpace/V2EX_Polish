062c6b0f1f9ab3e163252bbf50b17816699e3ecd
LeoKu <czc12580520@gmail.com>
2023-03-10 08:18:41 +0800

    WIP

--- src/userscripts/topic/comment.ts
+++ src/userscripts/topic/comment.ts
@@ -45,6 +45,7 @@ function handlingPopularComments() {
     let boundEvent = false
 
     const docClickHandler = (e: JQuery.ClickEvent) => {
+      // 通过判定点击的元素是否在评论框内来判断是否关闭评论框。
       if ($(e.target).closest(cmContent).length === 0) {
         // eslint-disable-next-line @typescript-eslint/no-use-before-define
         handleModalClose()
