4692748f932feaa490e1e0acc7c7cf59254890b4
LeoKu <czc12580520@gmail.com>
2023-05-14 14:07:34 +0800

    优化嵌套回复的楼层识别准确度

--- src/contents/topic/comment.ts
+++ src/contents/topic/comment.ts
@@ -294,11 +294,12 @@ export function handlingComments() {
             if (compareName === refName) {
               const firstRefFloor = refFloors?.at(0)
 
+              // 找到了指定回复的用户后，发现跟指定楼层对不上，则继续寻找。
               // 如果手动指定了楼层，那么就以指定的楼层为准（一般来说，由用户指定会更精确），否则就以第一个引用的用户的评论的楼层为准。
               if (firstRefFloor && firstRefFloor !== eachFloor) {
                 const targetIdx = commentDataList
                   .slice(0, j)
-                  .findIndex((data) => data.floor === firstRefFloor)
+                  .findIndex((data) => data.floor === firstRefFloor && data.memberName === refName)
 
                 if (targetIdx >= 0) {
                   $commentCells.eq(targetIdx).append(cellDom)
