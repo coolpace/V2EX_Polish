560a0b09ab29429a719def9e6221bfac509a3ee7
LeoKu <leokudev@gmail.com>
2023-09-16 16:52:14 +0800

    更新代码注释

--- extension/scripts/polyfill.js
+++ extension/scripts/polyfill.js
@@ -1,5 +1,5 @@
 if (!CSS.supports('selector(:has(*))')) {
-  // 检测到浏览器不支持 :has() 选择器，使用后备方案能切换至深色模式。
+  // 检测到如果浏览器不支持 `:has()` 选择器，则使用后备方案能切换至深色模式。
   if (document.querySelector('#Wrapper')?.classList.contains('Night')) {
     document.body.classList.add('v2p-theme-dark')
   }
