9d8243d6f94dab990247acc261c4f5a467cea69f
LeoKu <leokudev@gmail.com>
2024-01-04 21:42:32 +0800

    增加文件忽略规则

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,6 +1,8 @@
 module.exports = {
   customSyntax: 'postcss-scss',
+
   extends: [require.resolve('prefer-code-style/stylelint')],
+
   rules: {
     'selector-id-pattern': null,
     'selector-class-pattern': null,
@@ -8,5 +10,13 @@ module.exports = {
     'at-rule-no-unknown': null,
     'value-keyword-case': null,
   },
-  ignoreFiles: ['node_modules/**', 'dist/**', 'asset/**', 'extension/css/**', '**/*.min.css'],
+
+  ignoreFiles: [
+    'node_modules/**',
+    'dist/**',
+    'asset/**',
+    'extension/css/**',
+    '**/*.min.css',
+    'build*/**',
+  ],
 }
